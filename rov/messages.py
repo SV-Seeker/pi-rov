cf7ac835db844607e000f37f673b41662d8e3086
Patrick Forringer <patrick@forringer.com>
2018-04-22 17:50:54 -0500

    msg_id for tracking message type when new messages come in.

--- rov/messages.py
+++ rov/messages.py
@@ -17,9 +17,15 @@ class Message(Mapping):
     * attribute for a message key that is unique across all messages.
     """
     struct_keys = tuple()
+    msg_id = 0  # id must be less than 32767
 
     def __init__(self, packed_value=None, **kwargs):
-        self.format, self.make_kwargs = zip(*self.struct_keys)
+        packed_value = copy(packed_value)
+        kwargs = copy(kwargs)
+        keys = copy(self.struct_keys)
+        # add msg_id key to the front
+        keys = (('h', 'msg_id'),) + keys
+        self.format, self.make_kwargs = zip(*keys)
         self.struct = struct.Struct(''.join(self.format))
         if packed_value:
             # parse message from passed value
@@ -28,18 +34,31 @@ class Message(Mapping):
             # build message from kwargs
             self.make(**kwargs)
 
+    def upgrade(self):
+        """upgrade message to correct msg class"""
+        msg_id = self['msg_id']
+        # no need to upgrade
+        if self.msg_id != msg_id:
+            return self
+        # TODO: separate original bytes and packed_value
+        return msg_map[msg_id](packed_value=self.packed_value)
+
     def order_values(self, **kwargs):
         key_map = dict(zip(self.make_kwargs, range(len(self.make_kwargs))))
         return [kwargs[i] for i in sorted(kwargs, key=key_map.__getitem__)]
 
     def make(self, **kwargs):
         self.value = kwargs
+        kwargs.update({'msg_id': self.msg_id})
         self.packed_value = self.struct.pack(*self.order_values(**kwargs))
         return self.packed_value
 
     def parse(self, packed_value):
         self.packed_value = packed_value
-        unpacked_value = self.struct.unpack(packed_value)
+        size = self.struct.size
+        # crop off unneeded bits to prevent errors
+        unpacked_value = self.struct.unpack(packed_value[:size])
+        # left_over = packed_value[size:]
         self.value = self.assemble_value(unpacked_value)
         return self.value
 
@@ -88,3 +107,10 @@ class IMUMessage(Message):
 
 
 message_types = Message.__subclasses__()
+
+
+msg_map = {}
+for msg_id, msg_type in enumerate(message_types, start=1):
+    msg_type.msg_id = msg_id
+    # register mapping between msg_id and type
+    msg_map[msg_id] = msg_type
